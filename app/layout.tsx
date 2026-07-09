387e16d07b72b6cc16999755c76e30c4ac37aece
Navpreet Singh <navi.sim69@gmail.com>
2026-07-09 21:46:36 +0530

    hiii

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google"
 
-import "./globals.css"
+// import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
 
